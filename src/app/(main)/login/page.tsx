098ff9591c6816f276b64072fa872e0bf0caabb5
famadini <fernandoamadinigmail.com>
2023-08-12 14:52:26 -0300

    Nuevo Login

---
+++ src/app/(main)/login/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+import { NextPage } from 'next';
+import React from 'react';
+import Login from '../../../layout/Login';
+
+const LoginPage: NextPage = () => {
+  return (
+    <div>
+      <Login />
+    </div>
+  );
+};
+
+export default LoginPage;
