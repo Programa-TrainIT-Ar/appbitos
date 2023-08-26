46d91439718992014ea206f7f6e528d1accf7434
German <german.alejandrozulet@gmail.com>
2023-08-26 17:36:40 -0300

    Created modules for render snippets in medals/page.tsx removed unwanted files from tests

--- src/app/(main)/medals/page.tsx
+++ src/app/(main)/medals/page.tsx
@@ -1,11 +1,9 @@
 "use client"
-
 import React, { useEffect, useState } from "react";
 import { Medal } from "../../../types/medals";
 import { Dialog } from "primereact/dialog";
 import Image from "next/image";
 import MedalsService from "../../../service/MedalsService";
-
 
 const MedalsPage = () => {
 
@@ -24,55 +22,61 @@ const MedalsPage = () => {
     MedalsService.getMedals().then(setMedals)
   }, []);
 
+  function renderObtainedMedals() {
+    return (
+      medals
+        .filter((el: Medal) => el.obtained)
+        .slice(0, 3)
+        .map((el: Medal, index) => (
+          <div key={index} className="p-2 flex justify-content-center flex-column align-items-center">
+            <Image
+              loading="lazy"
+              src={el.image}
+              alt="Obtained Medal Picture"
+              onClick={() => {
+                setDisplayModal(true)
+                setCurrentMedal(el)
+              }}
+              width={150}
+              height={150}
+              className="w-10rem m-1" />
+            <p>{el.name}</p>
+          </div>
+        ))
+    )
+  }
 
-  
+  function renderLastObtainedMedals() {
+    return (
+      medals
+        .filter((el: Medal) => el.obtained === false)
+        .map((el: Medal, index) => (
+          <div key={index} className="sm:col-2 md:col-3 p-1 my-1 flex justify-content-center flex-column align-items-center">
+            <Image
+              onClick={() => {
+                setDisplayModal(true)
+                setCurrentMedal(el)
+              }}
+              src="https://www.svgrepo.com/show/465475/lock.svg"
+              alt="Medal Picture"
+              width={60}
+              height={60}
+              className="w-10rem m-1" />
+            <p>{el.name}</p>
+          </div>
+        ))
+    )
+  }
 
   return (
     <div className="flex h-screen" >
       <div className="flex flex-column  align-items-center">
         <h2 className="ml-4">Medallas obtenidas</h2>
-        {
-          medals
-            .filter((el: Medal) => el.obtained)
-            .slice(0, 3)
-            .map((el: Medal, index) => (
-              <div key={index} className="p-2 flex justify-content-center flex-column align-items-center">
-                <Image
-                  loading="lazy"
-                  src={el.image}
-                  alt="Obtained Medal Picture"
-                  onClick={() => {
-                    setDisplayModal(true)
-                    setCurrentMedal(el)
-                  }}
-                  width={150}
-                  height={150}
-                  className="w-10rem m-1" />
-                <p>{el.name}</p>
-              </div>
-            ))
-        }
+        {renderObtainedMedals()}
       </div>
+
       <div className="md:ml-7 grid overflow-y-scroll overflow-x-hidden">
-        {
-          medals
-            .filter((el: Medal) => el.obtained === false)
-            .map((el: Medal, index) => (
-              <div key={index} className="sm:col-2 md:col-3 p-1 my-1 flex justify-content-center flex-column align-items-center">
-                <Image
-                  onClick={() => {
-                    setDisplayModal(true)
-                    setCurrentMedal(el)
-                  }}
-                  src="https://www.svgrepo.com/show/465475/lock.svg"
-                  alt="Medal Picture"
-                  width={60}
-                  height={60}
-                  className="w-10rem m-1" />
-                <p>{el.name}</p>
-              </div>
-            ))
-        }
+        {renderLastObtainedMedals()}
 
         <Dialog
           header={currentMedal.name}
@@ -85,13 +89,13 @@ const MedalsPage = () => {
           dismissableMask={true}
           onHide={() => setDisplayModal(false)}
         >
-
           <div className="flex flex-column align-items-center  ">
             <Image className="my-4" src={currentMedal.image} alt="Current Medal Info" width={100} height={100} />
             <p className="m-0">{currentMedal.requirements}</p>
           </div>
 
         </Dialog>
+
       </div>
     </div>
   );
