4cc5f2cb5dc81c4228895fc690cc60de207f7572
Fer <fernanda23demarzo@gmail.com>
2023-08-26 13:43:53 -0300

    modifique users

--- stubs/data/users.js
+++ stubs/data/users.js
@@ -1,17 +1,18 @@
-const {faker} = require('@faker-js/faker');
+const { faker } = require('@faker-js/faker');
 
 function createRandomUser() {
     return {
         id: faker.string.uuid(),
         username: faker.internet.userName(),
+        firstName: faker.person.firstName(),
+        lastName: faker.person.lastName(),
         email: faker.internet.email(),
         avatar: faker.image.avatar(),
         password: faker.internet.password(),
-        birthdate: faker.date.birthdate(),
-        registeredAt: faker.date.past(),
+        birthdate: faker.date.birthdate({ min: 18, max: 65, mode: 'age' })
     };
 }
 
 module.exports = faker.helpers.multiple(createRandomUser, {
-    count: 5,
-})
+    count: 1
+});
