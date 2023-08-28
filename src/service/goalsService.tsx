f374c0d2caf004c401ebc52f428d57c647a6baf0
German <german.alejandrozulet@gmail.com>
2023-08-27 22:03:38 -0300

    Added few basic services tested in metas component

--- src/service/goalsService.tsx
+++ src/service/goalsService.tsx
@@ -7,6 +7,23 @@ export const goalsService = {
   getGoals: async (): Promise<Goal[]> => {
     return await fetch(`${BASE_URL}/goals`)
       .then(res => res.json())
+  },
+
+  createGoal: async (data: Goal): Promise<Object> => {
+    return await fetch(`${BASE_URL}/goals`, {
+      method: "POST",
+      body: JSON.stringify(data)
+    }).then(res => res.json())
+  },
+
+  deleteGoal: async (gid: string): Promise<Object> => {
+    return await fetch(`${BASE_URL}/goals/${gid}`, {
+      method: "DELETE",
+    }).then(res => res.json())
   }
+
+
+
+
 
 };
