3c8c9738a41d7d3f2ebeef1d8c0ee737457eda3f
Lucas Pirez <lucaspirez42@gmail.com>
2023-09-09 12:47:41 -0300

    add alert in button

--- src/components/calendar/MyAppointmentForm.tsx
+++ src/components/calendar/MyAppointmentForm.tsx
@@ -1,16 +1,22 @@
 import { AppointmentForm } from '@devexpress/dx-react-scheduler-material-ui';
 import { Button } from 'primereact/button';
+import { useState } from 'react';
 
 export default function MyAppointmentForm() {
+    const [buttonVisibility, setButtonVisibility] = useState(false);
+
+    const handleClick = () => {
+        alert('no implementado, la idea es que elimine todas las fechas repetidas si  existen, y no solo una');
+    };
     return (
         <>
             <AppointmentForm
-                onAppointmentDataChange={(e) => console.log(e)}
+                onAppointmentDataChange={(e) => setButtonVisibility(typeof e.rRule !== 'undefined')}
                 commandButtonComponent={({ id, onExecute, getMessage }) => {
                     if (id === 'deleteButton') {
                         return (
                             <>
-                                <Button label="all repeat" icon="pi pi-trash22, 213214" outlined />
+                                {buttonVisibility && <Button label="all repeat" icon="pi pi-trash22, 213214" outlined onClick={handleClick} />}
                                 {/* @ts-ignore */}
                                 <AppointmentForm.CommandButton onExecute={onExecute} getMessage={getMessage} id={id} />
                             </>
